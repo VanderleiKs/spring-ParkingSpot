817b9ec3b0e2f6d1a0d16198749d5dc9014369a4
vanderleik.silva <vanderleik.silva@procempa.com.br>
2023-03-01 14:09:05 -0300

    initial project

---
+++ src/main/java/app/entities/ParkingSpot.java
@@ -0,0 +1,43 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ParkingSpot {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	
+	private String name;
+
+	
+	public ParkingSpot() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+}
